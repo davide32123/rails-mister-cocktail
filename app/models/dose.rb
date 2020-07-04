a1c669806eb8d04e8dd16a9fd3d1c2ed2910d791
Davide Zanetto <davide.zanetto@gmail.com>
2020-07-04 11:40:00 +0900

    DB and models setup

---
+++ app/models/dose.rb
@@ -0,0 +1,7 @@
+class Dose < ApplicationRecord
+  validates :description, presence: true
+  validates :cocktail, presence: true, uniqueness: { scope: :ingredient }
+  validates :ingredient, presence: true
+  belongs_to :cocktail
+  belongs_to :ingredient
+end
